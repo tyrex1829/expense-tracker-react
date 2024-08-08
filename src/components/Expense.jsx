a347a8a6865e1b6ced689cd3b78ea80425ee3d87
tyrex1829 <sakshamkhare19@gmail.com>
2024-08-08 13:27:35 +0530

    inputs and button added

--- src/components/Expense.jsx
+++ src/components/Expense.jsx
@@ -1,9 +1,43 @@
-import React from "react";
+import { Button, Input, InputNumber } from "antd";
+import React, { useState } from "react";
 
 function Expense() {
+  const [expense, setExpense] = useState("");
+  const [amount, setAmount] = useState("");
+  const [list, setList] = useState([]);
+
+  const addExpense = () => {
+    if (!expense || !amount) {
+      return;
+    }
+  };
+
   return (
-    <div className="text-3xl font-bold text-center">
+    <div className="text-3xl font-bold flex flex-col mx-auto items-center justify-center gap-4 mt-9">
       <h1>Expense Tracker</h1>
+      <div className="max-w-screen-md">
+        <Input
+          onChange={(e) => {
+            setExpense(e.target.value);
+          }}
+          value={expense}
+          placeholder="Expense"
+        />
+        <InputNumber
+          className="w-full"
+          placeholder="Amount"
+          min={1}
+          value={amount}
+          onChange={(e) => {
+            setAmount(e.target.value);
+          }}
+        />
+        <div className="flex justify-center m-3">
+          <Button onClick={addExpense} type="primary">
+            Add Expense
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
