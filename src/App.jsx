dae881420d505861464f385c7db6a43da4cbd4af
tyrex1829 <sakshamkhare19@gmail.com>
2024-08-08 12:47:01 +0530

    test app

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <>Expense Tracker</>;
+  return (
+    <div className="text-3xl font-bold text-center">
+      <h1>Expense Tracker</h1>
+    </div>
+  );
 }
 
 export default App;
